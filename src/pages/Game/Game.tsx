7cad174ee8db77ea45fbff49f876273287478b16
Yuliia <julija.neshta@ro.ru>
2022-11-06 20:53:53 +0200

    fix: change structure for showing past score

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -5,7 +5,6 @@ import { DELAY, LETTERS } from '../../constants';
 
 import data from '../../services/data.json';
 import EndGame from '../EndGame';
-import Main from '../Main';
 import styles from './Game.module.scss';
 
 const Game: FC = () => {
@@ -14,35 +13,29 @@ const Game: FC = () => {
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [className, setClassName] = useState('');
   const [score, setScore] = useState('0');
-  const [futureScore, setFutureScore] = useState<string[]>([]);
-  const [pastScore, setPastScore] = useState<string[]>([]);
 
   const [questionIdx, setQuestionIdx] = useState(0);
 
   const { money, quiz } = data;
 
+  const indexCurrentScore = money?.findIndex(step => step.amount === score);
+
   useEffect(() => {
     const allAreTrue = (answers: boolean[]): boolean => answers.every(answer => answer);
     setStartGame(allAreTrue(answers));
+    setScore(money[questionIdx - 1]?.amount);
   }, [answers]);
-
-  useEffect(() => {
-    const currentMoney = money.find(money => money.id === questionIdx)?.amount;
-    if (currentMoney) {
-      setScore(currentMoney);
-    }
-  }, [questionIdx]);
 
   const handleClick = (answer: string, correctAnswer: string) => {
     setSelectedAnswer(answer);
-    //The time it takes for buttons to show styles
+    //Timeout it takes for buttons to show styles
     setTimeout(() => {
       setAnswers(prev => [...prev, answer === correctAnswer]);
     }, DELAY);
 
     setClassName(answer === correctAnswer ? styles.success : styles.error);
     if (answer === correctAnswer) {
-      //The time it takes for buttons to show styles
+      //Timeout for buttons to show styles
       setTimeout(() => {
         setQuestionIdx(prevState => prevState + 1);
       }, DELAY);
@@ -63,18 +56,6 @@ const Game: FC = () => {
       setStartGame(false);
     }
   }, [questionIdx]);
-
-  useEffect(() => {
-    const indexCurrentScore = money?.findIndex(step => step.amount === score);
-    // @ts-ignore
-    setFutureScore(money?.slice(0, indexCurrentScore));
-    // @ts-ignore
-    setPastScore(money?.slice(indexCurrentScore, money.length));
-    console.log('indexCurrentScore', indexCurrentScore);
-    console.log('pastScore', pastScore);
-    console.log('SCORE', score);
-    console.log('futureScore', futureScore);
-  }, [score]);
 
   return (
     <>
@@ -110,48 +91,25 @@ const Game: FC = () => {
               })}
           </div>
           <div className={styles.scores}>
-            {/*{money &&*/}
-            {/*  money.map(score => {*/}
-            {/*    return (*/}
-            {/*      <Button*/}
-            {/*        type="ghost"*/}
-            {/*        size="small"*/}
-            {/*        className={cn(styles.score, questionIdx === score.id ? styles.active : '')}*/}
-            {/*        key={score.id}*/}
-            {/*      >*/}
-            {/*        {score.amount}*/}
-            {/*      </Button>*/}
-            {/*    );*/}
-            {/*  })}*/}
-            {futureScore &&
-              futureScore.map(futureScore => {
-                return (
-                  <Button
-                    type="ghost"
-                    size="small"
-                    className={cn(styles.score, questionIdx === futureScore.id ? styles.active : '')}
-                    key={futureScore.id}
-                  >
-                    {futureScore.amount}
-                  </Button>
-                );
-              })}
-            {/*<Button type="ghost" size="small" className={cn(styles.score, styles.active)}>*/}
-            {/*  {score}*/}
-            {/*</Button>*/}
-            {pastScore &&
-              pastScore.map(pastScore => {
-                return (
-                  <Button
-                    type="ghost"
-                    size="small"
-                    className={cn(styles.score, styles.pastScore, questionIdx === pastScore.id ? styles.active : '')}
-                    key={pastScore.id}
-                  >
-                    {pastScore.amount}
-                  </Button>
-                );
-              })}
+            {money &&
+              money
+                .map(score => {
+                  return (
+                    <Button
+                      type="ghost"
+                      size="small"
+                      className={cn(
+                        styles.score,
+                        questionIdx === score.id ? styles.active : '',
+                        score.id > 0 && score.id <= indexCurrentScore ? styles.pastScore : '',
+                      )}
+                      key={score.id}
+                    >
+                      {score.amount}
+                    </Button>
+                  );
+                })
+                .reverse()}
           </div>
         </div>
       ) : (
